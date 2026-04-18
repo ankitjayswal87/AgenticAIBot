a19fffbc5dd794667cc8ce5b4ff8147ae0323ee7
Ankit Jayswal <ankitjayswal87@gmail.com>
2026-04-18 18:05:20 +0000

    accessing agent state, dynamic system prompt

--- agent_flask_new.py
+++ agent_flask_new.py
@@ -7,6 +7,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.tools import Tool
 from langchain.tools import tool, ToolRuntime
 from langchain.agents.middleware import dynamic_prompt, ModelRequest, before_model, after_model, SummarizationMiddleware
+from dataclasses import dataclass
 from langgraph.checkpoint.memory import InMemorySaver
 from flask import Flask, jsonify, request, send_file, redirect,has_request_context,make_response
 from flask_limiter import Limiter
@@ -26,19 +27,25 @@ os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 limiter = Limiter(get_remote_address,app=app,default_limits=["1000 per day", "100 per hour"])
 
-class CustomState(AgentState):
-    user_id: str
+@dataclass
+class Context:
+    user_name: str
+    
+# Dynamic prompts
+@dynamic_prompt
+def dynamic_system_prompt(request: ModelRequest) -> str:
+    user_name = request.runtime.context.user_name  
+    system_prompt = f"""You are a bus ticket booking agent. Be polite while speaking. Keep your answers short and easy to understand. Just book ticket between two cities. 
+Here required fields are from_city, to_city, journey_date and seats. First collect these information and confirm it with
+user via tool verify_confirm_ticket ,if user agrees then only book ticket Address the user as {user_name}."""
+    return system_prompt
 
 #verify the ticket details and confirm with user
 @tool(return_direct=True)
 def verify_confirm_ticket(from_city:str,to_city:str,journey_date:str,seats:str,runtime: ToolRuntime)->str:
     """Just verify all details received here"""
     print("JOURNEY_DATE:"+str(journey_date))
-    user_id = runtime.state["user_id"]
-    if user_id=="test123":
-        return f"Okay, Ankit you are going to book total {seats} tickets from {from_city} to {to_city} on date {journey_date}, Please confirm to book it!"
-    else:
-        return f"Okay, you are going to book total {seats} tickets from {from_city} to {to_city} on date {journey_date}, Please confirm to book it!"
+    return f"Okay, you are going to book total {seats} tickets from {from_city} to {to_city} on date {journey_date}, Please confirm to book it!"
 
 #ticket booking function
 @tool(return_direct=True)
@@ -51,23 +58,19 @@ def book_bus_ticket(from_city:str,to_city:str,journey_date:str,seats:str)->str:
     #return f"Your {seats} tickets are booked from {from_city} to {to_city} on date {journey_date} ,, the payment link has been sent to your mobile number, just click on it to pay to confirm tickets, Thank you!"
     return f"Your {seats} tickets from {from_city} to {to_city} on {journey_date} have been reserved. Please click the payment link sent to your mobile number, to complete the payment and confirm your booking. Thank you!"
 
-system_prompt="""You are a bus ticket booking agent. Be polite while speaking. Keep your answers short and easy to understand. Just book ticket between two cities. 
-Here required fields are from_city, to_city, journey_date and seats. First collect these information and confirm it with
-user via tool verify_confirm_ticket ,if user agrees then only book ticket"""
-
 agent = create_agent(
     model="gpt-4o-mini",
     tools=[verify_confirm_ticket,book_bus_ticket],
-    system_prompt=system_prompt,
     checkpointer=InMemorySaver(),
-    state_schema=CustomState,
     middleware=[
+        dynamic_system_prompt,
         SummarizationMiddleware(
             model="gpt-4o-mini",
             trigger=("messages",10),
             keep=("messages",4)
         )
-    ]
+    ],
+    context_schema=Context
     )
 
 @app.route('/agentic_ai/bus_booking',methods=['GET','POST'])
@@ -87,7 +90,7 @@ def bus_booking_api():
     elif llm_model=='openai':
         print('openai selected...')
         #response = agent.invoke({"messages":[{"role":"user","content":query}]},{"configurable": {"thread_id": thread_id}})
-        response = agent.invoke({"messages":[HumanMessage(content=query)],"user_id":"test123"},{"configurable": {"thread_id": thread_id}})
+        response = agent.invoke({"messages":[HumanMessage(content=query)]},{"configurable": {"thread_id": thread_id}},context=Context(user_name="Prahi Jayswal"))
         #print(len(response['messages']))
         #print(response['messages'])
         response = response['messages'][-1].content
